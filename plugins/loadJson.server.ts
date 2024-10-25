67a54dffe147b8d052fbd9dae2b37c5f3d2ee98e
Daria Andreeva <d.andreeva@grechka.digital>
2024-10-25 22:17:11 +0400

    updated load json plugin function

--- plugins/loadJson.server.ts
+++ plugins/loadJson.server.ts
@@ -2,7 +2,7 @@ import fs from 'fs'
 import path from 'path'
 
 export default defineNuxtPlugin((nuxtApp) => {
-  const filePath = path.resolve(__dirname, '../task_json.txt')
+  const filePath = path.resolve(process.cwd(), 'task_json.txt')
   const jsonData = fs.readFileSync(filePath, 'utf-8')
   const data = JSON.parse(jsonData)
 
